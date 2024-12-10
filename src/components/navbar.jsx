5d60b2aae8bfbe1d71ad3c2e524b2567b4742e6e
Swayam <swayambansal@outlook.com>
2024-12-10 19:51:56 +0530

    Created navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,37 @@
+import { Github } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="h-24  flex items-center justify-between px-24">
+      {/* Logo on the left */}
+      <div className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={250}
+          height={30}
+          className="h-11"
+        />
+      </div>
+
+      {/* Links on the right */}
+      <div className="flex space-x-10">
+        <a
+          href="https://github.com/swymbnsl/supasketch"
+          target="_blank"
+          className="text-black"
+        >
+          <Github />
+        </a>
+        <a href="/login" className="text-black font-semibold">
+          Login
+        </a>
+        <a href="/signup" className="text-black font-semibold">
+          Signup
+        </a>
+      </div>
+    </div>
+  );
+}
